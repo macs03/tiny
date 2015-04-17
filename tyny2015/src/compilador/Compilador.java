0dc8dd0a9327f3cbf87e0cdc75a50b9847cb42f3
yacson3287 <yacson.ramirez@gmail.com>
2015-04-17 00:00:33 -0430

    lee el codigo de archivo de prueba "entrada.txt" pasa por el parse()

--- tyny2015/src/compilador/Compilador.java
+++ tyny2015/src/compilador/Compilador.java
@@ -5,7 +5,7 @@ import java_cup.runtime.*;
 
 /**
  *
- *  @author Miguel CÃ¡rdenas
+ *  @author Miguel Cárdenas
  *  @author Yorman Castellanos
  *  @author Yacson Ramirez
  *  @author Jorge Gomez
@@ -14,19 +14,21 @@ import java_cup.runtime.*;
 public class Compilador {
 	
 	/***********
-	SymbolFactory es una nueva caracteristica que ha sido aï¿½adida a las version 11a de cup, la cual facilita la implementacion de clases Symbol personalizadas
-	, esto debido a que dicha clase no provee mucha informaciï¿½n de contexto que podria ser util para el analisis semantico o ayudar en la construccion del AST
+	SymbolFactory es una nueva caracteristica que ha sido a?adida a las version 11a de cup, la cual facilita la implementacion de clases Symbol personalizadas
+	, esto debido a que dicha clase no provee mucha informaci?n de contexto que podria ser util para el analisis semantico o ayudar en la construccion del AST
 	Mas informacion en: http//4thmouse.com/index.php/2007/02/15/using-custom-symbols-in-cup/
 	***********/
 
 	public static void main(String[] args) throws Exception {
+            
+            
 		@SuppressWarnings("deprecation")
 		SymbolFactory sf = new DefaultSymbolFactory();
 		parser parser_obj;
 		if (args.length==0) 
-			parser_obj=new parser(new Scanner(System.in,sf),sf);
+			parser_obj=new parser(new Scanner(new java.io.FileInputStream("entrada.txt"),sf),sf);
 		else 
-			parser_obj=new parser(new Scanner(new java.io.FileInputStream(args[0]),sf),sf);
+			parser_obj=new parser(new Scanner(new java.io.FileInputStream("entrada.txt"),sf),sf);
 
 		UtGen.debug=true; //NO muestro mensajes de depuracion del generador (UTGen) para que el codigo sea compatible con la version visual de la TM
 		//Para ver depuracion de analisis sintactico se debe ir al parser.java y colocar modoDepuracion en true
@@ -42,6 +44,8 @@ public class Compilador {
 	    //REALIZAR ACA ANALISIS SEMANTICO
 		Generador.setTablaSimbolos(ts);
 		Generador.generarCodigoObjeto(root);
+                    
+                    
 	}
 
 }
